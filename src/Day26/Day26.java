5000aedb4103d65d822efede8a0d348cdda8b60f
Krishna <krishna.21uad@mahendra.info>
2024-06-25 22:47:18 +0530

    Day 26 Completed

--- src/Day26/Day26.java
+++ src/Day26/Day26.java
@@ -1,19 +1,43 @@
 package Day26;
 
-import javax.swing.tree.TreeNode;
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
 
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
 public class Day26 {
     public static void main(String[] args) {
         Day26 day26 = new Day26();
-        day26.bstToGst(new TreeNode(4, new TreeNode(1, new TreeNode(0), new TreeNode(2, null, new TreeNode(3))), new TreeNode(6, new TreeNode(5), new TreeNode(7, null, new TreeNode(8))));
+    TreeNode root = new TreeNode(4);
+    root.left = new TreeNode(1);
+    root.right = new TreeNode(6);
+    root.left.left = new TreeNode(0);
+    root.left.right = new TreeNode(2);
+    root.left.right.right = new TreeNode(3);
+    root.right.left = new TreeNode(5);
+    root.right.right = new TreeNode(7);
+    TreeNode result = day26.bstToGst(root);
+    System.out.println(result.val);
     }
+
     public TreeNode bstToGst(TreeNode root) {
-        if (root != null) {
-            bstToGst(root.right);
-            sum += root.val;
-            root.val = sum;
-            bstToGst(root.left);
-        }
+        int val = 0;
+        if (root.right != null) bstToGst(root.right);
+        val = root.val = val + root.val;
+        if (root.left != null) bstToGst(root.left);
         return root;
     }
 }
