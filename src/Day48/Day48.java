bbf63c948433565c5f8c1faa090aee56ed6ff4ae
Krishna <krishna.21uad@mahendra.info>
2024-07-18 09:05:15 +0530

    Day 48 Completed

---
+++ src/Day48/Day48.java
@@ -0,0 +1,71 @@
+package Day48;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+class TreeNode {
+     int val;
+     TreeNode left;
+     TreeNode right;
+     TreeNode() {}
+     TreeNode(int val) { this.val = val; }
+     TreeNode(int val, TreeNode left, TreeNode right) {
+         this.val = val;
+         this.left = left;
+         this.right = right;
+     }
+}
+
+public class Day48 {
+    public static void main(String[] args) {
+        Day48 day48 = new Day48();
+        TreeNode root = new TreeNode(5);
+        root.left = new TreeNode(1);
+        root.right = new TreeNode(2);
+        root.left.left = new TreeNode(3);
+        root.left.right = new TreeNode(4);
+        root.right.left = new TreeNode(6);
+        System.out.println(day48.delNodes(root, new int[]{3, 5}));
+    }
+    public List<TreeNode> delNodes(TreeNode root, int[] to_delete) {
+        Map<Integer, TreeNode> res = new HashMap<>();
+        Set<Integer> to_delete_set = new HashSet<>();
+        for (int val : to_delete) {
+            to_delete_set.add(val);
+        }
+        res.put(root.val, root);
+
+        recursion(null, root, false, res, to_delete_set);
+
+        return new ArrayList<>(res.values());
+    }
+
+    private void recursion(TreeNode parent, TreeNode cur_node, boolean isleft, Map<Integer, TreeNode> res, Set<Integer> to_delete_set) {
+        if (cur_node == null) return;
+
+        recursion(cur_node, cur_node.left, true, res, to_delete_set);
+        recursion(cur_node, cur_node.right, false, res, to_delete_set);
+
+        if (to_delete_set.contains(cur_node.val)) {
+            res.remove(cur_node.val);
+
+            if (parent != null) {
+                if (isleft) {
+                    parent.left = null;
+                } else {
+                    parent.right = null;
+                }
+            }
+            if (cur_node.left != null) {
+                res.put(cur_node.left.val, cur_node.left);
+            }
+            if (cur_node.right != null) {
+                res.put(cur_node.right.val, cur_node.right);
+            }
+        }
+    }
+}
