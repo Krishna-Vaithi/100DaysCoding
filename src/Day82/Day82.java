c5b48a547a1be9065c18463645da67dbd8121796
Krishna <krishna.21uad@mahendra.info>
2024-08-21 07:39:10 +0530

    Day 82 Completed

--- src/Day82/Day82.java
+++ src/Day82/Day82.java
@@ -3,40 +3,37 @@ package Day82;
 public class Day82 {
     public static void main(String[] args) {
         Day82 day82 = new Day82();
-        System.out.println(day82.strangePrinter("aaabbb"));
+        System.out.println(day82.stoneGameII(new int[]{2, 7, 9, 4, 4}));
     }
 
-    public int strangePrinter(String s) {
+    public int stoneGameII(int[] piles) {
+        int dp[][][] = new int[piles.length + 1][piles.length + 1][2];
+        int ps[] = new int[piles.length + 1];
+        ps[0] = 0;
+        for (int i = 1; i < ps.length; i++) {
+            ps[i] = ps[i - 1] + piles[i - 1];
+        }
+        return helper(piles, 0, piles.length, 1, ps, 1, dp);
+    }
 
-        if (s == null || s.length() == 0) {
+    public int helper(int[] piles, int i, int j, int ch, int ps[], int m, int[][][] dp) {
+        if (i == j) {
             return 0;
         }
-
-        int n = s.length();
-        int[][] state = new int[n][n];
-
-        for (int i = 0; i < n; i++) {
-            state[i][i] = 1;
-        }
-
-        for (int i = n - 1; i >= 0; i--) {
-            for (int dist = 1; dist + i < n; dist++) {
-                int j = dist + i;
-                if (dist == 1) {
-                    state[i][j] = (s.charAt(i) == s.charAt(j)) ? 1 : 2;
-                    continue;
-                }
-                state[i][j] = Integer.MAX_VALUE;
-                for (int k = i; k + 1 <= j; k++) {
-                    int tmp = state[i][k] + state[k + 1][j];
-                    state[i][j] = Math.min(state[i][j], tmp);
-                }
-                if (s.charAt(i) == s.charAt(j)) {
-                    state[i][j]--;
-                }
+        if (dp[i][m][ch] != 0) return dp[i][m][ch];
+        int total = 0;
+        int ans = ch == 1 ? 0 : Integer.MAX_VALUE;
+        for (int k = 1; k <= 2 * m; k++) {
+            if (i + k > j) break;
+            total = total + piles[i + k - 1];
+            if (ch == 1) {
+                int sum = total + helper(piles, i + k, j, 0, ps, Math.max(m, k), dp);
+                ans = Math.max(sum, ans);
+            } else {
+                int sum = helper(piles, i + k, j, 1, ps, Math.max(m, k), dp);
+                ans = Math.min(ans, sum);
             }
         }
-
-        return state[0][n - 1];
+        return dp[i][m][ch] = ans;
     }
 }
